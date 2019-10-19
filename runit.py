3b9cab29db2f572157474173ea5ee55c2b2e5462
Justin Schreiber <jschreiber234@gmail.com>
2019-10-19 06:16:34 -0400

    changes

--- runit.py
+++ runit.py
@@ -10,13 +10,42 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 
-doc_ref = db.collection(u'users').document(u'alovelace')
+# doc_ref = db.collection(u'users').document(u'alovelace')
+# doc_ref.set({
+#     u'first': u'Ada',
+#     u'last': u'Lovelace',
+#     u'born': 1815
+# })
+
+doc_ref = db.collection(u'trivia'.document(u'Fact1'))
 doc_ref.set({
-    u'first': u'Ada',
-    u'last': u'Lovelace',
-    u'born': 1815
+    u'question_name': u'Do You Want To Build A Snowman?',
+    u'answer1': u'It Doesn\'t have to be a snowman',
+    u'answer2': u'It Doesn\'t have to be a snowman',
+    u'answer3': u'It Doesn\'t have to be a snowman',
+    u'answer4': u'It Doesn\'t have to be a snowman'
 })
 
+doc_ref = db.collection(u'profiles'.document(u'profile1'))
+doc_ref.set({
+    u'name': u'Amelia Bedeelia',
+    u'e-mail': u'fake@fake.com',
+    u'coordinator_status': u'y',
+    u'points': u'0',
+    u'uuid': u'8777777',
+    u'account_created': u'04-02-2019:12:12:12'
+})
+
+doc_ref = db.collection(u'events'.document(u'cleanup'))
+doc_ref.set({
+    u'event_name': u'Central Park Cleanup',
+    u'event_location': u'insert coordinates of central park here',
+    u'description': u'Help Clean NYC\'s Largest Park',
+    u'event_time': u'Saturday Oct 22,2017 3:30PM',
+    u'coordinator': u'Marcy Jenkins',
+    u'points_awarded': u'0',
+    u'event_id': u'77'
+})
 
 #prints everything in db
 # users_ref = db.collection(u'users')
